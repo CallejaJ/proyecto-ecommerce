845da2eea1fe335f90ab4ab4910c16cb85519860
Your Name <you@example.com>
2023-06-03 15:13:50 +0200

    login

--- JS/login.js
+++ JS/login.js
@@ -1,3 +1,9 @@
+// Declaro las variables y así son accesibles globalmente 
+
+    let loginForm;
+    let registrationForm;
+
+
 function inicializar() {
   console.log("onload se ha llamado");
 
@@ -10,7 +16,68 @@ function inicializar() {
   ]);
 
   menu.render();
+
+// Es una instancia a un nuevo formulario
+
+loginForm = new LoginForm();
+
+// Es la función de renderizado del formulario de login
+  loginForm.render();
 }
+
+// cierre del evento inicializar
+
+function LoginForm(){
+this.render = function (){
+  const loginFormDiv = document.getElementById("formulario-login");
+  loginFormDiv.innerHTML = `
+  
+        <h3>Iniciar sesion</h3>
+        <p>Accede con tus datos anteriores</p>
+        <input type="email" id="email" name="email" placeholder="E-mail"/>
+        <input type="password" id="password" name="contraseña" placeholder="Contraseña"/>
+        <button class="button" onclick = "loginForm.login()">Iniciar sesión</button>
+  `;
+  }; // cierre del método de renderizado del formulario login
+
+  // Método para loguearse
+
+  this.login = function () {
+    // variables locales del método login:
+
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const divContainer = document.getElementById("formulario-login");
+
+    let usuarioRegistrado = null;
+    const local = window.localStorage;
+
+    let tablaUsers = JSON.parse(local.getItem("tablaUsers"));
+    
+    
+    for(let i=0; i< tablaUsers.length; i++){
+      if(tablaUsers[i].email == email){
+        usuarioRegistrado = tablaUsers[i];
+        } // bucle que recorre el almacen local
+          
+      } // cierre del bucle for
+
+    if(usuarioRegistrado !== null){
+      if(password == usuarioRegistrado.password){
+      // happy path 
+      console.log("usuario logueado");
+      local.setItem("usuarioLogueado", JSON.stringify(usuarioRegistrado));
+      window.location = "index.html";
+      }
+      else if (password !== usuarioRegistrado.password){
+        console.log("unknown user");
+        const emailUnknown = document.getElementById("login-failed");
+        emailUnknown.innerHTML = `Contraseña incorrecta. Inténtalo de nuevo.`
+      }
+    } // cierre de la verificación de contraseña
+  } // cierre del método para loguearse "login"
+} // cierre de la clase LoginForm
+
 
 function Menu(links) {
   this.links = links;
